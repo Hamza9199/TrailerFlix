a33e1a6ee3abba68791030fd93210c2df37a2a9e
DzanB105 <dzan.babahmetovic.22@size.ba>
2024-05-19 21:26:02 +0200

    Ažurirana admin strana
    Dodane sve potrebne komponente i beta verzija stranice.

---
+++ adminStrana/komponente/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,39 @@
+import "./featuredInfo.css";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+
+export default function FeaturedInfo() {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Prihod</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">2,415 KM</span>
+          <span className="featuredMoneyRate">
+            -11.4 <ArrowDownward  className="featuredIcon negative"/>
+          </span>
+        </div>
+        <span className="featuredSub">Usporedba na zadnji mjesec</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">4,415 KM</span>
+          <span className="featuredMoneyRate">
+            -1.4 <ArrowDownward className="featuredIcon negative"/>
+          </span>
+        </div>
+        <span className="featuredSub">Usporedba na zadnji mjesec</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Cijena</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">2,225 KM</span>
+          <span className="featuredMoneyRate">
+            +2.4 <ArrowUpward className="featuredIcon"/>
+          </span>
+        </div>
+        <span className="featuredSub">Usporedba na zadnji mjesec</span>
+      </div>
+    </div>
+  );
+}
---
+++ adminStrana/stranice/home/newUser/NewUser.jsx
@@ -0,0 +1,52 @@
+import "./newUser.css";
+
+export default function NewUser() {
+  return (
+    <div className="newUser">
+      <h1 className="newUserTitle">Novi Korisnik</h1>
+      <form className="newUserForm">
+        <div className="newUserItem">
+          <label>Username</label>
+          <input type="text" placeholder="john" />
+        </div>
+        <div className="newUserItem">
+          <label>Ime i Prezime</label>
+          <input type="text" placeholder="John Smith" />
+        </div>
+        <div className="newUserItem">
+          <label>Email</label>
+          <input type="email" placeholder="john@gmail.com" />
+        </div>
+        <div className="newUserItem">
+          <label>Zaporka</label>
+          <input type="password" placeholder="password" />
+        </div>
+        <div className="newUserItem">
+          <label>Telefon</label>
+          <input type="text" placeholder="+1 123 456 78" />
+        </div>
+        <div className="newUserItem">
+          <label>Addresa</label>
+          <input type="text" placeholder="New York | USA" />
+        </div>
+        <div className="newUserItem">
+          <label>Spol</label>
+          <div className="newUserGender">
+            <input type="radio" name="gender" id="male" value="male" />
+            <label for="male">Muski</label>
+            <input type="radio" name="gender" id="female" value="female" />
+            <label for="female">Zenski</label>
+          </div>
+        </div>
+        <div className="newUserItem">
+          <label>Aktivan</label>
+          <select className="newUserSelect" name="active" id="active">
+            <option value="yes">Da</option>
+            <option value="no">Ne</option>
+          </select>
+        </div>
+        <button className="newUserButton">Kreiraj</button>
+      </form>
+    </div>
+  );
+}
