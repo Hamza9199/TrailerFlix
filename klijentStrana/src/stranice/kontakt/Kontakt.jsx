b2e7a9f6195dcbdbd2f2bd8ac30037bb0d10e923
Hamza Gačić <hamza.gacic.22@size.ba>
2024-05-30 23:32:13 +0200

    Mali bagovi rijeseni
    Kontakt stranica promjenuta, google mapa ubacena i radi, css malo kao urađen jer svakako ce se bolje sredit dodatno od kolege, i mala promjena na linku na mongo bazu u .env fajlu.

--- klijentStrana/src/stranice/kontakt/Kontakt.jsx
+++ klijentStrana/src/stranice/kontakt/Kontakt.jsx
@@ -1,54 +1,42 @@
 import React from 'react';
-import { Marker, Popup } from 'react-map-gl';
 import Footer from '../../komponente/footer/Footer';
 import Navbar from '../../komponente/navbar/Navbar.jsx';
 import style from './Kontakt.module.css';
-import ReactMapGL from 'react-map-gl';
-
-const position = [44.2010, 17.9037];
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiaGFtemE5OTk5OSIsImEiOiJjbHdoNWwwZWgwYmxuMmtwZnBjcTQ0OG15In0.hgKBbswuzfaGSjdg3nIlxg';
 
 function Kontakt() {
-    const [viewport, setViewport] = React.useState({
-        latitude: position[0],
-        longitude: position[1],
-        zoom: 13,
-        width: '100%',
-        height: 400
-    });
-
     return (
         <>
             <Navbar />
             <div className={style.container}>
-                <div className={style.mapContainer}>
-                    <ReactMapGL
-                        {...viewport}
-                        mapStyle="mapbox://styles/mapbox/streets-v11"
-                        onViewportChange={(viewport) => setViewport(viewport)}
-                        mapboxApiAccessToken={MAPBOX_TOKEN}
-                    >
-                        <Marker latitude={position[0]} longitude={position[1]}>
-                            <Popup>Zenica</Popup>
-                        </Marker>
-                    </ReactMapGL>
-                </div>
-                <div className={style.content}>
-                    <div className={style.kontaktContent}>
-                        <h1>Kontaktirajte nas</h1>
-                        <div className={style.contacnfo}>
-                            <h3>Informacije za kontakt</h3>
-                            <p>Adresa: Politehnički fakultet, Zenica</p>
-                            <p>Email: trailerflix.ba</p>
-                            <p>Telefon: +1234567890</p>
+                <div className={style.mapAndForm}>
+                    <div className={style.mapContainer}>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2699.787032686799!2d17.902471146679243!3d44.20037641233454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475ee24259b7e2c9%3A0xbbbe13ba46b02200!2sCampus%20of%20University%20of%20Zenica!5e0!3m2!1sen!2sba!4v1717103271957!5m2!1sen!2sba"
+                            width="350"
+                            height="250"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
+                    <div className={style.content}>
+                        <div className={style.kontaktContent}>
+                            <h1>Kontaktirajte nas</h1>
+                            <div className={style.contactInfo}>
+                                <h3>Informacije za kontakt</h3>
+                                <p>Adresa: Politehnički fakultet, Zenica</p>
+                                <p>Email: trailerflix.ba</p>
+                                <p>Telefon: +1234567890</p>
+                            </div>
+                            <form className={style.contactForm}>
+                                <h3>Pošaljite nam poruku</h3>
+                                <input type="text" placeholder="Ime" />
+                                <input type="email" placeholder="Email" />
+                                <textarea placeholder="Poruka"></textarea>
+                                <button type="submit">Pošalji</button>
+                            </form>
                         </div>
-                        <form className={style.contactForm}>
-                            <h3>Pošaljite nam poruku</h3>
-                            <input type="text" placeholder="Ime" />
-                            <input type="email" placeholder="Email" />
-                            <textarea placeholder="Poruka"></textarea>
-                            <button type="submit">Pošalji</button>
-                        </form>
                     </div>
                 </div>
             </div>
